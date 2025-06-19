1fe1b650942cf0a87ad01d35158c4308bf51411b
LukeRoberson <luke.robertson@networkdirection.net>
2025-06-19 13:05:36 +1000

    added plugin manager to init

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,3 +1,4 @@
 # This file marks the directory as a package.
 from .config import Config  # noqa: F401
 from .responses import error_response, success_response  # noqa: F401
+from .plugins import PluginManager  # noqa: F401
